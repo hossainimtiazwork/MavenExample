8a5b53a37f8e23f8228fb7c196efe935faca350e
Imtiaz <hossain.imtiaz.work@gmail.com>
2021-07-05 19:55:32 +0600

    A Test Commit for Jenkin Build to fail

--- Project/src/main/java/Main.java
+++ Project/src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("init main ");
+        System.out.println("init main ")
 
         Summation summation = new Summation();
         summation.sum(3 , 4);
